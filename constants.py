74ea29cf49beb512422c3ebd98bfd8e4f640ef31
Alex Pita <69311722+AlexPizzza@users.noreply.github.com>
2023-11-26 23:47:23 +0200

    first working version

--- constants.py
+++ constants.py
@@ -1,1 +1,28 @@
-EMULATOR_REGION = (0, 100, 100, 1000)
+EMULATOR_REGION = (5, 30, 555, 975)
+
+LANE_HEIGHT = 727
+STARTING_MIDDLE_LANE = 65
+DIFFERENCE_BETWEEN_LANES = 135
+
+LANES = [
+    {
+        'press': False,
+        'location': (LANE_HEIGHT, STARTING_MIDDLE_LANE),
+        'key': 'a'
+    },
+    {
+        'press': False,
+        'location': (LANE_HEIGHT, STARTING_MIDDLE_LANE + DIFFERENCE_BETWEEN_LANES),
+        'key': 's'
+    },
+    {
+        'press': False,
+        'location': (LANE_HEIGHT, STARTING_MIDDLE_LANE + 2 * DIFFERENCE_BETWEEN_LANES),
+        'key': 'd'
+    },
+    {
+        'press': False,
+        'location': (LANE_HEIGHT, STARTING_MIDDLE_LANE + 3 * DIFFERENCE_BETWEEN_LANES),
+        'key': 'f'
+    }
+]
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-import pyautogui
-from constants import EMULATOR_REGION
+import time
+import keyboard
 
-s = pyautogui.screenshot(region=EMULATOR_REGION)
-s.save('test.png')
+import d3dshot
+
+from constants import EMULATOR_REGION, LANES
+
+d = d3dshot.create(capture_output='numpy')
+
+print('Starting in 3...')
+time.sleep(3)
+while True:
+    start_time = time.time()
+    img = d.screenshot(region=EMULATOR_REGION)
+
+    for lane in LANES:
+        value = sum(img[lane['location']])
+        if value < 50 or img[lane['location']][0] == 0:
+            keyboard.press(lane['key'])
+            time.sleep(.1)
+        else:
+            keyboard.release(lane['key'])
+
+    if keyboard.is_pressed('q'):
+        break
